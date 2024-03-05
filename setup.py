a2e6815f87c3cb6296544834c17bd8044fbf6ce1
Zhengqi Gao <zhengqi@mit.edu>
2024-03-05 09:51:35 -0500

    add installation

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='sampler',
+    version='0.0',
+    packages=find_packages(),
+    url='https://github.com/zhengqigao/Sampler/sampler/',
+    license='MIT',
+    author='Zhengqi Gao',
+    author_email='zhengqi@mit.edu',
+    description='A python package for various sampling methods',
+    install_requires=[
+        # List of dependencies, if any
+    ],
+)
