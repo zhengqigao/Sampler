36bf1b2cd59f64ad36de15846be09200231e19aa
fdu-dkw <22307130211@m.fudan.edu.cn>
2024-03-19 20:35:08 +0800

    Merge branch 'local_dev' into dev

--- test/test1_importance_sampling.py
+++ test/test1_importance_sampling.py
@@ -15,7 +15,7 @@ class MultiGauss(Distribution):
         self.mean = torch.tensor(mean, dtype=torch.float32)
         self.std = torch.tensor(std, dtype=torch.float32)
         self.dim = len(mean)
-        self.const = 1.0
+        self.norm = 1.0
 
     def sample(self, num_samples: int) -> torch.Tensor:
         return torch.randn((num_samples, self.dim)) * self.std + self.mean
@@ -42,7 +42,7 @@ class MultiGaussDenser(Distribution):
         self.mean = torch.tensor(mean, dtype=torch.float32)
         self.std = torch.tensor(std, dtype=torch.float32)
         self.dim = len(mean)
-        self.const = 33.3
+        self.norm = 1 / 33.3
 
     def sample(self, num_samples: int) -> torch.Tensor:
         return torch.randn((num_samples, self.dim)) * self.std + self.mean
@@ -66,39 +66,56 @@ class MultiGaussDenser(Distribution):
 # normal case
 target = MultiGauss(mean=test_mean, std=[1, 1, 1])
 proposal = MultiGauss(mean=[0, 0, 0], std=[1, 1, 1])
+proposal.norm = 1.0
 results = importance_sampling(10000, target, proposal, lambda x: x)
-print("Test mean:", results)  # it works
+print("Test mean:", results)
+# it works
+
 
 # self.norm = None, actually 1.0
 target.norm = None
 results = importance_sampling(10000, target, proposal, lambda x: x)
-print("Test mean:", results)  # it works as if norm == 1.0
+print("Test mean:", results)
+# it works as if norm == 1.0
 
-# self.norm = 33.3, actually 1.0
-target.norm = 33.3
+
+# self.norm = 1/33.3, actually 1.0
+target.norm = 1 / 33.3
 results = importance_sampling(10000, target, proposal, lambda x: x)
-print("Test mean:", results)  # it works as if norm == 1.0
+print("Test mean:", results)
+# fails, gets approximately [-0.0009, 0.0009, 0.0004]
+
 
 print("")
-# MultiGaussDenser, 
-# whose evaluate_density() returns 33.3x larger value
-# self.norm = 33.3 in fact
-target_denser = MultiGaussDenser(mean=test_mean, std=[1, 1, 1])
-proposal = MultiGauss(mean=[0, 0, 0], std=[1, 1, 1])
-results = importance_sampling(10000, target_denser, proposal, lambda x: x)
-print("Test mean:", results)  # it works as if norm == 1.0
 
-# self.norm = None, actually 33.3
+
+# MultiGaussDenser,
+# whose evaluate_density() returns 33.3x larger value
+# self.norm = 1/33.3 in fact
+target_denser = MultiGaussDenser(mean=test_mean, std=[1, 1, 1])
+target_denser.norm = 1 / 33.3
+results = importance_sampling(10000, target_denser, proposal, lambda x: x)
+print("Test mean:", results)
+# fails, gets approximately [-0.03, 0.03, 0.015]
+
+
+
+# self.norm = None, actually 1/33.3
 target_denser.norm = None
 results = importance_sampling(10000, target_denser, proposal, lambda x: x)
-print("Test mean:", results)  # it works as if norm == 1.0
+print("Test mean:", results)
+# it works as if norm == 1.0
 
-# self.norm = 1.0, actually 33.3
+
+# self.norm = 1.0, actually 1/33.3
 target_denser.norm = 1.0
 results = importance_sampling(10000, target_denser, proposal, lambda x: x)
-print("Test mean:", results)  # it works as if norm == 1.0
+print("Test mean:", results)  # fails, gets approximately [-33, 33, 17]
+
 
 print("")
+
+
 # torch.distributions.multivariate_normal.MultivariateNormal
 target2 = Wrapper(MultivariateNormal(test_mean, torch.eye(3)))
 proposal2 = Wrapper(MultivariateNormal(torch.zeros(3), torch.eye(3)))
