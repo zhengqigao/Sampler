dd43ec9f542348d98d7e883996f7fa10f8b5c89f
fdu-dkw <22307130211@m.fudan.edu.cn>
2024-03-20 09:32:22 +0800

    test0 tests mul_factor<=0 and mul_factor=None

--- test/test0_basic.py
+++ test/test0_basic.py
@@ -6,13 +6,13 @@ from typing import Optional
 from torch.distributions.multivariate_normal import MultivariateNormal
 from sampler.distribution import Wrapper
 
+
 class MultiGauss(Distribution):
     def __init__(self, mean, std):
         super().__init__()
         self.mean = torch.tensor(mean, dtype=torch.float32)
         self.std = torch.tensor(std, dtype=torch.float32)
         self.dim = len(mean)
-
 
     def sample(self, num_samples: int) -> torch.Tensor:
         return torch.randn((num_samples, self.dim)) * self.std + self.mean
@@ -32,27 +32,37 @@ class MultiGauss(Distribution):
             )
 
 
+instance1 = MultiGauss([1, 2], [1, 1])
+
 try:
-    instance1 = MultiGauss([1, 2], [1, 1]) # should raise an error,as we haven't defined the mul_factor or div_factor
+    instance1.mul_factor = 0  # should raise an error, as mul_factor should be positive
     print(instance1.mul_factor)
 except Exception as e:
     print("Error raised as expected, w/ error: ", e)
 
+try:
+    # should raise an error,as we haven't defined the mul_factor or div_factor
+    print(instance1.mul_factor)
+except Exception as e:
+    print("Error raised as expected, w/ error: ", e)
 
-instance1.mul_factor = 2.0 # should be 1.0, but here we give mul_factor = 2.0 to test if div_factor can be returned correctly
-print(instance1.div_factor) # should return 0.5
-print(instance1.mul_factor)
-instance1.mul_factor = 3.0 # say we find 2.0 is wrong, it should be mul_factor = 3.0
-print(instance1.div_factor)
-print(instance1.mul_factor)
-instance1.div_factor = 3.0 # say we find actually it is div_factor = 3.0, not mul_factor = 3.0
-print(instance1.div_factor)
-print(instance1.mul_factor)
+# should be 1.0, but here we give mul_factor = 2.0 to test if div_factor can be returned correctly
+instance1.mul_factor = 2.0
+print(instance1.mul_factor, instance1.div_factor)  # should return 2.0 0.5
+
+# say we find 2.0 is wrong, it should be mul_factor = 3.0
+instance1.mul_factor = 3.0
+print(instance1.mul_factor, instance1.div_factor)  # 3.0 0.3333333333333333
+
+# say we find actually it is div_factor = 3.0, not mul_factor = 3.0
+instance1.div_factor = 3.0
+print(instance1.mul_factor, instance1.div_factor)  # 0.3333333333333333 3.0
+
+# remove mul_factor
+instance1.mul_factor = None
+print(instance1.mul_factor, instance1.div_factor)  # None None
 
 # remove the field mul_factor of instance1
 instance2 = MultiGauss([1, 2], [1, 1])
 instance2.div_factor = 2.0
-print(instance2.mul_factor) # should return 0.5
-print(instance2.div_factor)
-
-
+print(instance2.mul_factor, instance2.div_factor)  # should return 0.5 2.0
