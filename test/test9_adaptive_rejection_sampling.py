ac04e359fdc4029b94c2308948cdc3545a845d40
Coone21 <17300750105@fudan.edu.cn>
2024-04-22 21:31:51 +0800

    modified ARS

--- test/test9_adaptive_rejection_sampling.py
+++ test/test9_adaptive_rejection_sampling.py
@@ -2,22 +2,7 @@ import torch
 
 from sampler.base import *
 from sampler._common import Distribution
-
-
-class MultiGauss(Distribution):
-    def __init__(self, mean, std):
-        super().__init__()
-        self.mean = mean if isinstance(mean, torch.Tensor) else torch.tensor(mean, dtype=torch.float32)
-        self.std = std if isinstance(std, torch.Tensor) else torch.tensor(std, dtype=torch.float32)
-        self.dim = len(mean)
-        self.mul_factor = 1.0
-
-    def sample(self, num_samples: int) -> torch.Tensor:
-        return torch.randn((num_samples, self.dim)) * self.std + self.mean
-
-    def log_prob(self, x: torch.Tensor) -> torch.Tensor:
-        return -0.5 * (torch.sum(((x - self.mean) / self.std) ** 2, dim=1) + torch.log(
-            2 * torch.pi * self.std * self.std).sum())
+from test_common_helper import MultiGauss
 
 
 test_mean = [1]
