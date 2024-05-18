2c58fbfe785d4dd594623499b83183c9181486b2
fdu-dkw <22307130211@m.fudan.edu.cn>
2024-05-18 23:52:55 +0800

    try plot RS

--- test/test2_RS_squeezing.py
+++ test/test2_RS_squeezing.py
@@ -1,12 +1,39 @@
+import numpy as np
+import matplotlib.pyplot as plt
 from sampler.base import *
 from sampler._common import Distribution
 from test_common_helper import MultiGauss, DensityFunc
 
-test_mean = [-1, 1, 0.5]
-target = MultiGauss(mean=test_mean, std=[1, 1, 1])
-proposal = MultiGauss(mean=[0, 0, 0], std=[1.2, 1.2, 1.2])
-squeezing = MultiGauss(mean=test_mean, std=[0.6, 0.6, 0.6])
-results, info = rejection_sampling(10000, target,
-                                   proposal=proposal, k=40,
-                                   squeezing=squeezing, k_squeezing=0.7)
+def show(target, results, info):
+    x = torch.linspace(-5, 5, 100)
+    y = torch.linspace(-5, 5, 100)
+    xx, yy = torch.meshgrid(x, y)
+    grid_data = torch.cat((yy.reshape(-1, 1), xx.reshape(-1, 1)), dim=1)
+    value = target(grid_data)
+    z = torch.exp(value).reshape(100,-1)
+    plt.figure()
+    plt.title(f"Mean: {torch.mean(results, dim=0)}\nRejection rate: {info['rejection_rate']}")
+    plt.pcolormesh(x, y, z[:-1, :-1], cmap='summer')
+    plt.colorbar()
+    plt.scatter(results[:, 0], results[:, 1], marker='.', color='red', alpha=0.1)
+    plt.show()
+
+test_mean = [-1, 0.5]
+target = MultiGauss(mean=test_mean, std=[1, 1])
+proposal = MultiGauss(mean=[0, 0], std=[1.2, 1.2])
+squeezing = MultiGauss(mean=[1, -1], std=[0.9, 0.9])
+results, info = rejection_sampling(2000, target,
+                                   proposal=proposal, k=6,
+                                   squeezing=squeezing, k_squeezing=0.03)
 print(f"Mean: {torch.mean(results, dim=0)}\tRejection rate: {info['rejection_rate']}\tSize: {results.shape}")
+show(target, results, info)
+
+test_mean = [-1, 0.5]
+target = MultiGauss(mean=test_mean, std=[1, 1])
+proposal = MultiGauss(mean=[0, 0], std=[1.2, 1.2])
+squeezing = MultiGauss(mean=[1, -1], std=[0.9, 0.9])
+results, info = rejection_sampling(2000, target,
+                                   proposal=proposal, k=6,
+                                   squeezing=squeezing, k_squeezing=0.5)
+print(f"Mean: {torch.mean(results, dim=0)}\tRejection rate: {info['rejection_rate']}\tSize: {results.shape}")
+show(target, results, info)
