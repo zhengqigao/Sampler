6b120596ca12ec74bd2e5ec62a76c0aa8c015152
Zhengqi Gao <zhengqi@mit.edu>
2024-05-24 11:06:18 -0400

    add MAF

--- test/test15_maf.py
+++ test/test15_maf.py
@@ -16,22 +16,22 @@ from sklearn import datasets
 from sampler.functional import KLDenLoss, KLGenLoss, ScoreDenLoss
 
 
-
 def test_maf():
     dim = 4
-    num_trans = 2
+    num_trans = 10
     mg = MultiGauss(mean=[0] * dim, std=[1] * dim)
     flowtransform = MAF(dim=dim, num_trans=num_trans,
-                        made = nn.ModuleList(MADE(hidden_dims=[dim, 20, dim]) for _ in range(num_trans)),
-                        p_base= mg)
+                        made=nn.ModuleList(MADE(hidden_dims=[dim, 20, 2 * dim]) for _ in range(num_trans)),
+                        p_base=mg)
     print(flowtransform.p_base)
     x = torch.rand(100, dim)
     x.requires_grad = True
     x_, log_prob = flowtransform.backward(*flowtransform.forward(x, 0))
     print(f"diff = {torch.max(torch.abs(x - x_)):.3e}, log_prob = {log_prob.abs().max():.3e}")
     print(x[0,])
-def run_density_matching_example():
 
+
+def run_density_matching_example():
     potential_func = PotentialFunc("potential6")
 
     # show poential_function
@@ -54,15 +54,15 @@ def run_density_matching_example():
     dim = 2
     mg = MultiGauss(mean=[0] * dim, std=[1] * dim)
     module = MAF(dim=dim, num_trans=num_trans,
-                        made = nn.ModuleList(MADE(hidden_dims=[dim, 20, dim]) for _ in range(num_trans)),
-                        p_base= mg).to(device)
+                 made=nn.ModuleList(MADE(hidden_dims=[dim, 20, 2 * dim]) for _ in range(num_trans)),
+                 p_base=mg).to(device)
 
-    optimizer = torch.optim.Adam(module.parameters(), lr=0.01)
+    optimizer = torch.optim.Adam(module.parameters(), lr=0.0001)
     scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=1000, gamma=0.5)
-    max_iter = 4000
+    max_iter = 10
     loss_list = []
     batch_size = 1000
-    criterion1 = KLDenLoss(log_p = lambda x: -potential_func(x))
+    criterion1 = KLDenLoss(log_p=lambda x: -potential_func(x))
 
     for i in range(max_iter):
         loss = criterion1(module, batch_size)
@@ -78,7 +78,6 @@ def run_density_matching_example():
         optimizer.step()
         scheduler.step()
         print(f"iter {i}, loss: {loss.item()}")
-
 
     plt.figure()
     samples, log_prob = module.sample(50000)
@@ -98,19 +97,19 @@ def run_density_matching_example():
     plt.show()
 
 
-def run_generation_example(plot_or_save = 'plot', device='cpu'):
+def run_generation_example(plot_or_save='plot', device='cpu'):
     device = torch.device(device)
 
     num_trans = 30
     dim = 2
     mg = MultiGauss(mean=[0] * dim, std=[1] * dim)
     module = MAF(dim=dim, num_trans=num_trans,
-                        made = nn.ModuleList(MADE(hidden_dims=[dim, 20, dim]) for _ in range(num_trans)),
-                        p_base= mg).to(device)
+                 made=nn.ModuleList(MADE(hidden_dims=[dim, 20, 2 * dim]) for _ in range(num_trans)),
+                 p_base=mg).to(device)
 
     optimizer = torch.optim.Adam(module.parameters(), lr=0.01)
     scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=500, gamma=0.5)
-    num_steps = 4000
+    num_steps = 10
     criterion = KLGenLoss()
     loss_list = []
     for i in range(num_steps):
@@ -129,8 +128,8 @@ def run_generation_example(plot_or_save = 'plot', device='cpu'):
     plt.figure()
     plt.scatter(samples[:, 0], samples[:, 1])
     plt.title("generated samples")
-    plt.xlim(-1.5,2.5)
-    plt.ylim(-1.5,1.5)
+    plt.xlim(-1.5, 2.5)
+    plt.ylim(-1.5, 1.5)
     if plot_or_save == 'save':
         plt.savefig('tmp_generated.png')
 
@@ -138,8 +137,8 @@ def run_generation_example(plot_or_save = 'plot', device='cpu'):
     x, _ = datasets.make_moons(n_samples=1000, noise=0.1)
     plt.scatter(x[:, 0], x[:, 1])
     plt.title("real samples")
-    plt.xlim(-1.5,2.5)
-    plt.ylim(-1.5,1.5)
+    plt.xlim(-1.5, 2.5)
+    plt.ylim(-1.5, 1.5)
     plt.figure()
     plt.plot(loss_list)
     plt.title('loss list')
@@ -148,8 +147,6 @@ def run_generation_example(plot_or_save = 'plot', device='cpu'):
         plt.show()
 
 
-
-
-# test_radial_flow()
+# test_maf()
 # run_density_matching_example()
 run_generation_example('plot','cpu')
