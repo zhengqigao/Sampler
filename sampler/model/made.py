30e4a0a7135f5e8341b8b64dab37a11baeb19447
Zhengqi Gao <zhengqi@mit.edu>
2024-05-24 10:19:22 -0400

    add MADE

--- sampler/model/made.py
+++ sampler/model/made.py
@@ -16,14 +16,25 @@ class MADE(nn.Module):
     Masked Autoencoder for Distribution Estimation (MADE). Code is copied and refined based on https://github.com/e-hulten/maf/blob/master/made.py
 
     """
+
     def __init__(self, hidden_dims: List,
                  activation: Optional[nn.Module] = nn.ReLU(),
-                 gaussian: Optional[bool] = False,
                  order: Optional[Union[Tuple, List, torch.Tensor]] = None):
+        r"""
+
+        Args:
+            hidden_dims (List): The hidden dimensions of the MADE. The first element represents MADE input
+                                dimension, the number of random variables, and the number of distributions. The last element represents MADE output dimension, the numebr of parameters for all distributions. The output dimensions must be integers
+                                multiples of the input dimensions. When hidden_dims[-1] = k * hidden_dims[0], k represents the number of parameters associated with each distribution. For instance, when k=2, it can represent the means and stds of Gaussian distributions.
+            activation (Optional[nn.Module], optional): The activation function. Defaults to nn.ReLU().
+            order (Optional[Union[Tuple, List, torch.Tensor]], optional): The order of the input dimensions. Defaults to None.
+        """
         super().__init__()
+        assert hidden_dims[-1] % hidden_dims[0] == 0, ("The output dimension should be a multiple of the "
+                                                       "input dimension in MADE.")
+
         self.hidden_dims = hidden_dims
         self.activation = activation
-        self.gaussian = gaussian
         self.order = order
 
         self.hidden_layers = nn.ModuleList(
@@ -40,14 +51,22 @@ class MADE(nn.Module):
     @order.setter
     def order(self, value: Optional[Union[Tuple, List, torch.Tensor]] = None):
         if value is not None:
-            order = torch.Tensor(value)
-            if order.min().item() != 0 or order.max().item() != self.hidden_dims[0] - 1 or len(torch.unique(order)) != self.hidden_dims[0]:
+            order = torch.Tensor(value).type(torch.int)
+            if order.min().item() != 0 or order.max().item() != self.hidden_dims[0] - 1 or len(torch.unique(order)) != \
+                    self.hidden_dims[0]:
                 raise ValueError("The order should be a permutation of 0, 1, ..., hidden_dim[0]-1.")
         else:
             order = torch.arange(self.hidden_dims[0])
         self._order = order
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
+        r"""
+        Forward pass of the MADE model. It returns a tensor of shape (batch_size, hidden_dims[-1]). The parameters
+        for the 0-th distribution locate at index = 0, hidden_dims[0], 2*hidden_dims[0], ...(k-1)*hidden_dims[0].
+
+        Args:
+            x (torch.Tensor): The input tensor of shape (batch_size, hidden_dims[0]).
+        """
         for i in range(len(self.hidden_layers) - 1):
             x = self.activation(self.hidden_layers[i](x))
         x = self.hidden_layers[-1](x)
@@ -58,14 +77,16 @@ class MADE(nn.Module):
         self.mask[0] = torch.Tensor(self.order) if self.order is not None else torch.arange(D)
 
         for l in range(1, L - 1):
-            low = self.mask[l-1].min().item()
+            low = self.mask[l - 1].min().item()
             size = self.hidden_dims[l]
             self.mask[l] = torch.randint(low=low, high=D - 1, size=(size,))
-        self.mask[L - 1] = self.mask[0] # the output layer is the same as the input layer
+        self.mask[L - 1] = self.mask[0]  # the output layer is the same as the input layer
 
         for l in range(L - 1):
             if l == L - 2:
                 mask_matrix = self.mask[l + 1].unsqueeze(1) > self.mask[l].unsqueeze(0)
+                k = int(self.hidden_dims[-1] / self.hidden_dims[0])
+                mask_matrix = mask_matrix.repeat(k,1)
             else:
                 mask_matrix = self.mask[l + 1].unsqueeze(1) >= self.mask[l].unsqueeze(0)
             self.mask_matrix.append(mask_matrix)
--- test/test14_made.py
+++ test/test14_made.py
@@ -12,21 +12,29 @@ import numpy as np
 from sklearn import datasets
 import numpy as np
 
-dim = 10
-order = range(dim) # np.random.permutation(range(dim))
-model = MADE(hidden_dims=[dim, 4, 2, dim])
+# set random seed
+# torch.manual_seed(0)
+
+dim = 5
+multiple = 1
+order = np.random.permutation(range(dim))
+model = MADE(hidden_dims=[dim, 20, multiple * dim], order=order)
 
 bs = 1
 x = torch.rand(bs, dim)
 res = model(x)
 
-ind = 3
+ind = 1
 x[0, ind] += 10  # Correctly modify the element in x
 res2 = model(x)
 diff = torch.abs(res - res2)
 
 # Find indices where diff is not zero
 indices = diff==0
-print(f"Final NN output doesn't change indices = {torch.arange(dim).view(-1,dim)[indices]}")
+print(f"Calculated: Final NN output doesn't change indices = {torch.arange(multiple * dim).view(-1,multiple * dim)[indices]}")
 print(f"order = {order}")
-print(f"change index = {ind}")
+print(f"change ind={ind}")
+print(f"Golden: the following indices shouldn't change (repeat {multiple} times, w/ period {dim}): {torch.arange(dim)[order<=order[ind]]}")
+
+print(model.mask)
+print(model.mask_matrix)
