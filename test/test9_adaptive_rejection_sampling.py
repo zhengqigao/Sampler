3d9686613cb17c7731c053ec046e87e539de13f6
Coone21 <17300750105@fudan.edu.cn>
2024-06-02 14:48:46 +0800

    Update test9_adaptive_rejection_sampling.py

--- test/test9_adaptive_rejection_sampling.py
+++ test/test9_adaptive_rejection_sampling.py
@@ -6,16 +6,28 @@ from test_common_helper import MultiGauss
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-
 test_mean = [-1]
 test_std = [0.5]
-lower = -1.3
-upper = -0.2
 target = MultiGauss(mean=test_mean, std=test_std)
 target.mul_factor = None
+print("=====================================")
+print(f"Test 1: mean {test_mean}\tstd {test_std}")
+print("=====================================")
 
+lower = -1.3
+upper = -0.2
 results, info = adaptive_rejection_sampling(10000, target, lower, upper)
+print(f"Mean: {torch.mean(results, dim=0)}\tRejection rate: {info['rejection_rate']}\t#Iteration: {info['iteration_count']}\tSize: {results.shape}")
 
+plt.subplot(1,2,1)
+plt.hist(results.cpu().detach().numpy(), bins=50)
+plt.subplot(1,2,2)
+sns.kdeplot(data=results.cpu().detach().numpy())
+plt.show()
+
+lower = -3
+upper = 1
+results, info = adaptive_rejection_sampling(10000, target, lower, upper)
 print(f"Mean: {torch.mean(results, dim=0)}\tRejection rate: {info['rejection_rate']}\t#Iteration: {info['iteration_count']}\tSize: {results.shape}")
 
 plt.subplot(1,2,1)
@@ -25,17 +37,29 @@ sns.kdeplot(data=results.cpu().detach().numpy())
 plt.show()
 
 
-print("=====================================")
-
 test_mean = [1]
 test_std = [1]
-lower = 0.5
-upper = 1.5
 target = MultiGauss(mean=test_mean, std=test_std)
 target.mul_factor = None
+print("\n=====================================")
+print(f"Test 2: mean {test_mean}\tstd {test_std}")
+print("=====================================")
 
+lower = 0.5
+upper = 1.5
 results, info = adaptive_rejection_sampling(10000, target, lower, upper)
+print(f"Mean: {torch.mean(results, dim=0)}\tRejection rate: {info['rejection_rate']}\t#Iteration: {info['iteration_count']}\tSize: {results.shape}")
 
+plt.subplot(1,2,1)
+plt.hist(results.cpu().detach().numpy(), bins=50)
+plt.subplot(1,2,2)
+sns.kdeplot(data=results.cpu().detach().numpy())
+plt.show()
+
+
+lower = 0
+upper = 3
+results, info = adaptive_rejection_sampling(10000, target, lower, upper)
 print(f"Mean: {torch.mean(results, dim=0)}\tRejection rate: {info['rejection_rate']}\t#Iteration: {info['iteration_count']}\tSize: {results.shape}")
 
 plt.subplot(1,2,1)
